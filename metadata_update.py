3f55bc4e186df814d771ce9d3b9ae7dec242165e
jbwexler <jbwexler@stanford.edu>
2025-11-17 11:22:51 -0800

    Refactor, inspired by https://github.com/OpenNeuroOrg/openneuro/blob/master/scripts/check-github-sync

--- metadata_update.py
+++ metadata_update.py
@@ -1,8 +1,13 @@
-import requests
 import os
 import pandas as pd
 import logging
-import sys
+import httpx
+import gql
+import stamina
+from gql import Client, gql as gql_query
+from gql.transport.httpx import HTTPXTransport
+from rich.progress import Progress, TextColumn, BarColumn, MofNCompleteColumn
+from operator import itemgetter
 
 
 DTYPES = {
@@ -18,62 +23,66 @@ AGE_DICT = {
     (51, 65): "51-65",
     (66, 1000): "66+",
 }
-HEADERS = {
-    "Accept-Encoding": "gzip, deflate, br",
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Connection": "keep-alive",
-    "DNT": "1",
-    "Origin": "https://openneuro.org",
-}
 
-QUERY = """
-{
+ENDPOINT = "https://openneuro.org/crn/graphql"
+QUERY = gql_query("""
+query DatasetsWithLatestSnapshots($count: Int, $after: String) {
+  datasets(
+    first: $count,
+    after: $after,
+    orderBy: { created: ascending }
+    filterBy: { public: true }
+  ) {
     edges {
-        cursor,
-        node {
-            id,
-            publishDate,
-            latestSnapshot {
-                tag,
-                created,
-                size,
-                dataset {
-                    name, 
-                    metadata {
-                      trialCount,
-                      studyDesign,
-                      studyDomain,
-                      studyLongitudinal,
-                      dataProcessed,
-                      species,
-                      associatedPaperDOI,
-                      openneuroPaperDOI,
-                      dxStatus,
-                      affirmedConsent,
-                      affirmedDefaced
-                    }
-                }, 
-                description {
-                    SeniorAuthor,
-                    DatasetType
-                },
-                summary {
-                    subjects,
-                    modalities, 
-                    secondaryModalities, 
-                    subjectMetadata {
-                        age
-                    }, 
-                    tasks,
-                    dataProcessed
-                }
+      node {
+        id
+        publishDate
+        latestSnapshot {
+          tag
+          created
+          hexsha
+          size
+          dataset {
+            name
+            metadata {
+              trialCount
+              studyDesign
+              studyDomain
+              studyLongitudinal
+              dataProcessed
+              species
+              associatedPaperDOI
+              openneuroPaperDOI
+              dxStatus
+              affirmedConsent
+              affirmedDefaced
             }
+          }
+          description {
+            SeniorAuthor
+            DatasetType
+          }
+          summary {
+            subjects
+            modalities
+            secondaryModalities
+            subjectMetadata {
+              age
+            }
+            tasks
+            dataProcessed
+          }
         }
+      }
     }
+    pageInfo {
+      hasNextPage
+      endCursor
+      count
+    }
+  }
 }
-""".replace("\n", "")
-
+""")
 
 logger = logging.getLogger(__name__)
 
@@ -120,7 +129,7 @@ def format_name(name: str) -> str | None:
         return name
 
 
-def handle_error(func, error):
+def handle_error(func, error=TypeError):
     """Returns None if the specified error occurs.
     Otherwise returns output of function."""
     try:
@@ -129,34 +138,44 @@ def handle_error(func, error):
         return None
 
 
-def perform_query(next_cur=None) -> dict:
-    """Performs query and outputs response as dict. If next_cur is not provided,
-    gets the first chunk of datasets. Otherwise, gets the next chunk."""
-    if next_cur:
-        query_dict = (
-            f'{{"query": "query testq{{datasets(after: \\"{next_cur}\\") '
-            + QUERY
-            + '}"}'
-        )
-    else:
-        query_dict = '{"query":"query testq{datasets ' + QUERY + '}"}'
+@stamina.retry(on=(httpx.HTTPError, gql.transport.exceptions.TransportConnectionFailed))
+def get_page(client: Client, count: int, after: str | None) -> dict:
+    return client.execute(QUERY, variable_values={"count": count, "after": after})
 
-    num_attempts = 3
-    for i in range(num_attempts):
+
+def get_dataset_count(client: Client) -> int:
+    response = get_page(client, 0, None)
+    return response["datasets"]["pageInfo"]["count"]
+
+
+def get_all_metadata(
+    client: Client, progress: Progress, task_id, metadata_list
+) -> None:
+    """Fetches datasets from GraphQL API, processes them and appends to metadata_list"""
+    page_info = {"hasNextPage": True, "endCursor": None}
+
+    while page_info["hasNextPage"]:
         try:
-            response = requests.post(
-                "https://openneuro.org/crn/graphql", headers=HEADERS, data=query_dict
-            )
-            return response.json()
-        except requests.exceptions.JSONDecodeError:
-            pass
+            result = get_page(client, 100, page_info["endCursor"])
+        except gql.transport.exceptions.TransportQueryError as e:
+            logger.error("GraphQL query error")
+            if e.data is not None:
+                result = e.data
 
-    logger.error("Request failed %s times: %s" % (num_attempts, response))
-    logger.error("Exiting.")
-    sys.exit(1)
+        edges, page_info = itemgetter("edges", "pageInfo")(result["datasets"])
+
+        for edge in edges:
+            if edge is None:
+                continue
+
+            metadata = create_metadata_dict(edge)
+            metadata_list.append(metadata)
+
+            dataset_id = edge["node"]["id"]
+            progress.update(task_id, advance=1, dataset=dataset_id)
 
 
-def create_data_dict(in_data: dict) -> dict:
+def create_metadata_dict(in_data: dict) -> dict:
     """Creates a dict containing fields to be outputted for particular dataset."""
     dataset_field = in_data["node"]["latestSnapshot"]["dataset"]
     summary_field = in_data["node"]["latestSnapshot"]["summary"]
@@ -173,27 +192,22 @@ def create_data_dict(in_data: dict) -> dict:
         "dataset_name": dataset_field["name"],
         "made_public": handle_error(
             lambda: in_data["node"]["publishDate"][:10],
-            TypeError,
         ),
         "most_recent_snapshot": in_data["node"]["latestSnapshot"]["created"][:10],
         "num_subjects": handle_error(
             lambda: str(len(summary_field["subjects"])),
-            TypeError,
         ),
         "modalities": handle_error(
             lambda: format_modalities(
                 summary_field["secondaryModalities"] + summary_field["modalities"]
             ),
-            TypeError,
         ),
         "dx_status": dataset_field["metadata"]["dxStatus"],
         "ages": handle_error(
             lambda: format_ages(summary_field["subjectMetadata"]),
-            TypeError,
         ),
         "tasks": handle_error(
             lambda: ", ".join(summary_field["tasks"]),
-            TypeError,
         ),
         "num_trials": dataset_field["metadata"]["trialCount"],
         "study_design": dataset_field["metadata"]["studyDesign"],
@@ -216,7 +230,6 @@ def create_data_dict(in_data: dict) -> dict:
         ),
         "size_gb": handle_error(
             lambda: round(in_data["node"]["latestSnapshot"]["size"] / (1024**3), 2),
-            TypeError,
         ),
         "dataset_type": in_data["node"]["latestSnapshot"]["description"]["DatasetType"],
     }
@@ -224,35 +237,29 @@ def create_data_dict(in_data: dict) -> dict:
     return out_data
 
 
-def main():
+def main() -> int:
     logging.basicConfig()
+    client = Client(transport=HTTPXTransport(url=ENDPOINT))
 
-    # Initial query
-    response = perform_query()
+    count = get_dataset_count(client)
 
-    output = []
-    while True:
-        ds_data_list = response["data"]["datasets"]["edges"]
+    metadata_list = []
+    with Progress(
+        TextColumn(
+            "[progress.description]{task.description} {task.fields[dataset]:8s}"
+        ),
+        BarColumn(),
+        MofNCompleteColumn(),
+    ) as progress:
+        task = progress.add_task("Fetching", total=count, dataset="...")
+        get_all_metadata(client, progress, task, metadata_list)
 
-        for ds_data in ds_data_list:
-            if not ds_data:
-                continue
-
-            row = create_data_dict(ds_data)
-            output.append(row)
-
-        # A response with < 25 datasets implies last query
-        if len(ds_data_list) < 25:
-            break
-
-        # Next query
-        response = perform_query(next_cur=ds_data_list[-1]["cursor"])
-
-    df = pd.DataFrame(output)
+    df = pd.DataFrame(metadata_list)
     df = df.astype(dtype=DTYPES)
     df = df.set_index("accession_number")
     df = df.sort_index()
     df = df.groupby(df.index).first()  # Remove rows with duplicated accession numbers
+
     df.to_csv("metadata.csv", mode="w+")
 
 
